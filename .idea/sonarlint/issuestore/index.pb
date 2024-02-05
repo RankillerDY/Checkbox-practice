64dd9fc0ec6550f1e16ca20fc872c750fefc05e9
RankillerDY <rankillerdy@gmail.com>
2024-02-05 23:04:23 +0700

    finish

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+p
+@app/src/main/java/com/example/checkboxpractice/MainActivity.java,3\d\3dba0d11e571d21f2a093b88a2792beb503cd3cb
--- app/src/main/java/com/example/checkboxpractice/MainActivity.java
+++ app/src/main/java/com/example/checkboxpractice/MainActivity.java
@@ -3,12 +3,39 @@ package com.example.checkboxpractice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private CheckBox checkBoxVisibility;
+    private TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        checkBoxVisibility = findViewById(R.id.checkBox_visibility);
+        text = findViewById(R.id.txtHelloWorld);
+        boolean isChecked = checkBoxVisibility.isChecked();
+
+        updateTextVisibility(isChecked);
+
+        checkBoxVisibility.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                updateTextVisibility(isChecked);
+            }
+        });
+    }
+
+    private void updateTextVisibility(boolean isChecked) {
+        if(isChecked) {
+            text.setVisibility(View.VISIBLE);
+        } else {
+            text.setVisibility(View.INVISIBLE);
+        }
     }
 }
